1ee939cac5a835bdf331d7f6cc6e928982a0242d
Deeka Wong <huangdijia@gmail.com>
2022-08-29 15:14:13 +0800

    Adds test cases

---
+++ tests/DeptTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of youduphp/youdu.
+ *
+ * @link     https://github.com/youduphp/youdu
+ * @document https://github.com/youduphp/youdu/blob/main/README.md
+ * @contact  huangdijia@gmail.com
+ */
+use YouduPhp\Youdu\Application;
+
+beforeEach(function () {
+    $config = makeConfig();
+    $this->dept = (new Application($config))->dept;
+});
+
+it('asserts dept', function () {
+    expect($this->dept)->toBeInstanceOf(\YouduPhp\Youdu\Kernel\Dept\Client::class);
+});
