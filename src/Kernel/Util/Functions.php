c24e9d6a844a86f7a8935c06b2b2e042ef738a03
huangdijia <huangdijia@gmail.com>
2022-08-29 21:01:28 +0800

    Adds helper functions tap() and with()

---
+++ src/Kernel/Util/Functions.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of youduphp/youdu.
+ *
+ * @link     https://github.com/youduphp/youdu
+ * @document https://github.com/youduphp/youdu/blob/main/README.md
+ * @contact  huangdijia@gmail.com
+ */
+namespace YouduPhp\Youdu\Kernel\Util;
+
+/**
+ * Call the given Closure with the given value then return the value.
+ *
+ * @param mixed $value
+ */
+function tap($value, callable $callback = null)
+{
+    if (is_null($callback)) {
+        return new HigherOrderTapProxy($value);
+    }
+
+    $callback($value);
+
+    return $value;
+}
+
+/**
+ * Return the given value, optionally passed through the given callback.
+ *
+ * @template TValue
+ *
+ * @param TValue $value
+ * @param  (callable(TValue): TValue)|null  $callback
+ * @return TValue
+ */
+function with($value, callable $callback = null)
+{
+    return is_null($callback) ? $value : $callback($value);
+}
