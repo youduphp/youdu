b0184f857d2aafdfdc149354ed640a8ee9d04ad1
Deeka Wong <huangdijia@gmail.com>
2022-08-29 16:42:17 +0800

    Adds test cases

---
+++ tests/GroupTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of youduphp/youdu.
+ *
+ * @link     https://github.com/youduphp/youdu
+ * @document https://github.com/youduphp/youdu/blob/main/README.md
+ * @contact  huangdijia@gmail.com
+ */
+use YouduPhp\Youdu\Application;
+
+beforeEach(function () {
+    $config = makeConfig();
+    $this->group = (new Application($config))->group;
+});
+
+it('assert get group info', function () {
+    expect($this->group)->toBeInstanceOf(\YouduPhp\Youdu\Kernel\Group\Client::class);
+});
---
+++ tests/SessionTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of youduphp/youdu.
+ *
+ * @link     https://github.com/youduphp/youdu
+ * @document https://github.com/youduphp/youdu/blob/main/README.md
+ * @contact  huangdijia@gmail.com
+ */
+use YouduPhp\Youdu\Application;
+
+beforeEach(function () {
+    $config = makeConfig();
+    $this->session = (new Application($config))->session;
+});
+
+it('assert get session info', function () {
+    expect($this->session)->toBeInstanceOf(\YouduPhp\Youdu\Kernel\Session\Client::class);
+});
